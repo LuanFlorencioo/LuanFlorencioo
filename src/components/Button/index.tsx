5c069cee2774b27857bd02013df87af22f581e88
Luan Florencio <luanflorencio.dev@gmail.com>
2023-05-19 00:42:42 -0300

    feat(Button): add menu icon

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
-import { FaLinkedin, FaGithub, FaWhatsapp } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaWhatsapp, FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 import { iButtonProps } from "@/interfaces";
 import styles from "./styles.module.css";
 
@@ -7,6 +8,8 @@ const Button = ({color, icon, value, clickEvent}: iButtonProps) => {
     linkedin: <FaLinkedin size={22} />,
     github: <FaGithub size={22} />,
     whatsapp: <FaWhatsapp size={22} />,
+    menuBars: <FaBars size={20} />,
+    menuClose: <IoClose size={20} />,
   }
 
   const setColor = {
--- src/interfaces/Button/index.ts
+++ src/interfaces/Button/index.ts
@@ -1,6 +1,6 @@
 interface iButtonProps {
   color: "orange" | "white"
-  icon?: "linkedin" | "github" | "whatsapp"
+  icon?: "linkedin" | "github" | "whatsapp" | "menuBars" | "menuClose"
   value?: string
   clickEvent?: () => any
 }
