470c0c062a24a8ecea7112ff43c1c918cf6021c1
Luan Florencio <luanflorencio.dev@gmail.com>
2023-05-20 21:34:37 -0300

    feat(TechsSection): create TechsSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import {
   Header,
   Home,
+  TechsSection,
 } from "@/components";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Header />
       <Home />
+      <TechsSection />
     </>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Button from "./Button";
 import Header from "./Header";
 import Home from "./Home";
+import TechsSection from "./TechsSection";
 
 export {
   Button,
   Header,
   Home,
+  TechsSection,
 }
