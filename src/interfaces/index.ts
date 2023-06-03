82067f09b444176a340cb036c4f203bcfcf27b54
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-03 18:47:52 -0300

    fix(TechsSection): remove splide lib

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,6 +2,7 @@ import { iPropChildren } from "./children";
 import { iButtonProps } from "./Button";
 import { iLink } from "./Header";
 import { iProjectCardProps, iRepo } from "./Projects";
+import { iTechData } from "./Techs";
 
 export type {
   iPropChildren,
@@ -9,4 +10,5 @@ export type {
   iLink,
   iProjectCardProps,
   iRepo,
+  iTechData,
 }
